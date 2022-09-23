d864e288459c6caaeda57e96633570afb6198cc9
unknown <pedrolol73@gmail.com>
2022-09-23 02:03:41 -0300

    feat: returning minimum shipping amount

--- src/entity/Shipping.ts
+++ src/entity/Shipping.ts
@@ -2,6 +2,8 @@ import Item from './Item';
 import Order from './Order';
 
 export default class Shipping {
+	readonly MINIMUM_SHIPPING_AMOUNT = 10;
+
 	total: number;
 
 	constructor(readonly order: Order, readonly distance: number) {
@@ -9,11 +11,13 @@ export default class Shipping {
 	}
 
 	calculateShipping() {
-		return this.order.items.reduce((total, item) => {
+		const total = this.order.items.reduce((total, item) => {
 			const orderItem = this.order.orderItems.find((orderItem) => orderItem.itemId === item.id);
 			if (!orderItem) throw new Error('Cannot find order item.');
 			return (total += this.calculateItemShipping(item, orderItem.quantity));
 		}, 0);
+
+		return total < this.MINIMUM_SHIPPING_AMOUNT ? this.MINIMUM_SHIPPING_AMOUNT : total;
 	}
 
 	private calculateItemShipping(item: Item, quantity: number) {
--- test/entity/Shipping.test.ts
+++ test/entity/Shipping.test.ts
@@ -6,10 +6,20 @@ import Shipping from '../../src/entity/Shipping';
 test('Deve calcular o frete de um pedido', () => {
 	const cpf = new Cpf('835.179.250-04');
 	const order = new Order(cpf);
-	order.addItem(new Item('1', 'Camiseta', 100, 200, 100, 50, 3), 1); // R$ 30,00 * 1
-	order.addItem(new Item('2', 'Calça', 50, 150, 100, 100, 3), 3); // R$ 30,00 * 3
-	order.addItem(new Item('3', 'Meia', 20, 50, 50, 50, 1), 2); // R$ 10,00 * 2
+	order.addItem(new Item('1', 'Camiseta', 100, 200, 100, 50, 3), 1);
+	order.addItem(new Item('2', 'Calça', 50, 150, 100, 100, 3), 3);
+	order.addItem(new Item('3', 'Meia', 20, 50, 50, 50, 1), 2);
 	const distance = 1000;
 	const shipping = new Shipping(order, distance);
 	expect(shipping.total).toBe(140);
+});
+
+test('Deve retornar o valor mínimo do frete de um pedido', () => {
+	const cpf = new Cpf('835.179.250-04');
+	const order = new Order(cpf);
+	order.addItem(new Item('3', 'Meia', 20, 5, 5, 5, 0.5), 1);
+	const distance = 1000;
+	const shipping = new Shipping(order, distance);
+	const minimumShippingAmount = 10;
+	expect(shipping.total).toBe(minimumShippingAmount);
 });
